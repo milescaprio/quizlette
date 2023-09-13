68acc33624def9b7c3af001277be541e820c4a67
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-09-12 21:25:40 -0700

    wait reduce does NOT work

---
+++ lib.py
@@ -0,0 +1,67 @@
+from functools import reduce
+import random
+
+def nest(*fns):
+    #reduce(fun, seq, initial) calls fun of each pair of elements in seq; pushing to the left and moving the next one in
+    #Here we are calling each function in fns as the sequence, so we need to manually feed the function into the function
+    return lambda x, y: reduce(lambda v, f: f(v), fns, x)
+
+def lowercased(a, b):
+    return a.lower() == b.lower()
+
+def stripped(a, b):
+    return a.strip() == b.strip()
+
+class QuestionType:
+    FRQ = 0
+    MCQ = 1
+
+class Question:
+    term = ""
+    definition = ""
+    wrongAnswers = [] # Only for MCQ
+    answers_count = 4 # Only for MCQ
+    answer_check = lambda a, b : a == b # Only for FRQ
+    question_type = QuestionType.MCQ
+    def __init__(self, term, definition, questiontype, answer_count = 4, wrongAnswers=[], answer_check=lambda a, b : a == b):
+        self.term = term
+        self.definition = definition
+        self.question_type = questiontype
+        self.wrongAnswers = wrongAnswers
+        self.answer_check = answer_check
+    def with_answer_check_function(self, answer_check):
+        copy = self
+        copy.answer_check = answer_check
+        return copy
+    def with_wrong_answers(self, wrongAnswers):
+        copy = self
+        copy.wrongAnswers = wrongAnswers
+        return copy
+
+def run_game(questions):
+    question_scores = [0 for i in questions]
+    while True:
+        question = random.choice(questions)
+        if question.question_type == QuestionType.FRQ:
+            answer = input(question.term+": ")
+            if question.answer_check(answer, question.definition):
+                question_scores[questions.index(question)] += 1
+            else:
+                print("Incorrect, it was: " + question.definition)
+                question_scores[questions.index(question)] -= 1
+        elif question.question_type == QuestionType.MCQ:
+            answers = random.sample(question.wrongAnswers, questions.answer_count - 1)
+            answers.append(question.definition)
+            random.shuffle(answers)
+            answer = answers.find(question.definition)
+            print("A: " + answers[0])
+            print("B: " + answers[1])
+            print("C: " + answers[2])
+            print("D: " + answers[3])
+            answer = input("Answer: ")
+            if question.answer_check(answer, question.definition):
+                question_scores[questions.index(question)] += 1
+            else:
+                question_scores[questions.index(question)] -= 1
+                print("Incorrect, it was: " + question.definition)
+    # print("Score: " + str(score) + "/" + str(len(questions))
