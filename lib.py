befb8998fc1d49fd9208b00bb62621e276f772f8
PunkyMunky64 <46407361+PunkyMunky64@users.noreply.github.com>
2023-09-16 17:31:42 -0700

    Save, keyword, printing improvements

--- lib.py
+++ lib.py
@@ -17,6 +17,12 @@ def stripped(a):
 def filtered_equals(filter_fn):
     return lambda a, b : filter_fn(a) == filter_fn(b)
 
+def save_filename(fn):
+    return "__"+fn+"_save__.py"
+
+def save_filename_extensionless(fn):
+    return "__"+fn+"_save__"
+
 class QuestionType:
     FRQ = 0
     MCQ = 1
@@ -25,7 +31,7 @@ class Question:
     term = ""
     definition = ""
     wrongAnswers = [] # Only for MCQ
-    answers_count = 4 # Only for MCQ
+    answer_count = 4 # Only for MCQ
     answer_check = lambda a, b : a == b # Only for FRQ
     question_type = QuestionType.MCQ
     reinforcements_needed = 3
@@ -55,30 +61,42 @@ class Game:
     fn = None
     question_scores = []
     do_save = True
-    def __init__(self, questions, fn):
+    quit_keywords = ["Quit", "q"]
+    override_keywords = ["Override", "o"]
+    def __init__(self, questions, fn, okeywords = ["Override", "o"], qkeywords = ["Quit", "q"]):
         self.questions = questions
         self.fn = fn
+        self.override_keywords = okeywords
+        self.quit_keywords = qkeywords
 
     def __del__(self):
         if self.fn is not None and self.do_save:
-            with open("__"+self.fn+"_save__.py", "w") as f:
+            with open(save_filename(self.fn), "w") as f:
                 f.write("s = " + str(self.question_scores) + "\n")
 
     def add(self, question):
         self.questions.append(question)
 
     def run(self):
-        print("Type \"Override\" at any point to make your answer to the last question correct.")
+        ostr = ""
+        for o in self.override_keywords:
+            ostr += '"' + o + '" or '
+        ostr = ostr[:-4]
+        qstr = ""
+        for q in self.quit_keywords:
+            qstr += '"' + q + '" or '
+        qstr = qstr[:-4]
+        print("Type " + ostr + " at any point to make your answer to the last question correct.")
         print("(The purpose of this program is to help you learn, not to test you, so discounting.")
         print("certain mistakes is up to you. However, it's not recommended to override unless")
         print("there was actually a good reason.)")
-        print("Type \"Quit or Q\" at any point to quit the game, and save your progress")
+        print("Type "+qstr+" at any point to quit the game, and save your progress")
         self.question_scores = [0 for i in self.questions]
-        if os.path.isfile(self.fn+".txt"):
+        if os.path.isfile(save_filename(self.fn)):
             print("It looks like you weren't done... would you like to attempt to continue where you left off (load scores from file)?")
             answer = input("Y/R/G (Yes / Restart / Guest Session): ").lower().strip()
             if answer == "y":
-                temp = importlib.import_module("__"+self.fn+"_save__").s
+                temp = importlib.import_module(save_filename_extensionless(self.fn)).s
                 if len(temp) == len(self.questions):
                     self.question_scores = temp
                 else:
@@ -92,7 +110,8 @@ class Game:
         while True:
             question = random.choice(self.questions)
             if self.question_scores[self.questions.index(question)] == question.reinforcements_needed:
-                continue #TODO: Improve this
+                continue
+                # Future: add better method for learning besides random question selection
             questions_cache.append(question)
             if question.question_type == QuestionType.FRQ:
                 answer = input(question.term+": ")
@@ -109,25 +128,25 @@ class Game:
                     print("Incorrect, it was: " + question.definition)
                     self.question_scores[self.questions.index(question)] -= 1
             elif question.question_type == QuestionType.MCQ:
-                answers = random.sample(question.wrongAnswers, self.questions.answer_count - 1)
+                answers = random.sample(question.wrongAnswers, question.answer_count - 1)
                 answers.append(question.definition)
                 random.shuffle(answers)
-                answer = answers.find(question.definition)
+                answer = answers.index(question.definition)
+                print(question.term + ": ")
                 print("A: " + answers[0])
                 print("B: " + answers[1])
                 print("C: " + answers[2])
                 print("D: " + answers[3])
                 answer = input("Answer: ")
-                if answer.lower().strip() == "quit":
+                if answer.lower().strip() == "quit" or answer.lower().strip() == "q":
                     return
                 elif answer.lower().strip() == "override":
                     self.question_scores[questions_cache[-2]] += 2
                     print("Overrided")
-                if question.answer_check(answer, question.definition):
+                if answer.lower().strip() == "abcd"[answers.index(question.definition)]:
                     self.question_scores[self.questions.index(question)] += 1
                     if self.question_scores[self.questions.index(question)] == question.reinforcements_needed:
                         print("You have mastered " + question.term + ", with {} points".format(question.reinforcements_needed))
                 else:
                     self.question_scores[self.questions.index(question)] -= 1
                     print("Incorrect, it was: " + question.definition)
-        # print("Score: " + str(score) + "/" + str(len(questions))
